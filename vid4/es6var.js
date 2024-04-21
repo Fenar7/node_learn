6508da6f2d857686d2f6cbfbb5d9dc0eee07579a
Fenar Khan <fenarkhan@gmail.com>
2024-04-21 09:44:23 +0530

    3

---
+++ vid4/es6var.js
@@ -0,0 +1,11 @@
+var num = 10
+var num = 20// the value get reassignes
+//but if we use let we can only create a variable with a unique name
+
+let hello = 10
+// let hello = 20 //this will give error
+
+
+
+
+console.log(num)

f93215c13558aaa1722947a6b5477df655dcc04a
Fenar Khan <fenarkhan@gmail.com>
2024-04-21 13:05:15 +0530

    6

--- vid6/app.js
+++ vid6/app.js
@@ -1,6 +1,10 @@
 var http = require('http')
+var fs = require('fs')
 
 http.createServer(function (req,res){
-    res.write('package')
-    res.end()
+    fs.readFile('sample.html',function(err, data){ // file is read if success we get data else err object
+        res.writeHead(200,{'Content-Type' : 'text/html'}) // 200 is the code for webrequest success we want to pass an object specifying the type
+        res.write(data)
+        res.end()// AFTER file is read execute 
+    })//async model so we add a callback function
 }).listen(7000)
